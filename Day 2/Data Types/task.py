4cecef6aae57063865f9d8ce25b817fc6432dc62
Felipe Suguiura <felipesuguiura.pro@gmail.com>
2024-08-19 11:11:07 +0100

    Início do projeto

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,21 @@
-len("Hello")
+from math import trunc
+
+print("Hello"[4])
+print("Hello"[-1])
+
+# String
+print("123" + "456")
+
+# integers
+print(123 + 456)
+
+# Large Integers
+print(123_456_789)
+print(123,456,789)
+
+# Boolean
+print(True)
+print(False)
+
+# Float
+print(123.45)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,8 @@
-len(12345)
+#print(len(str(12345)))
+
+#print(type("Hello"))
+#print(type(123))
+#print(type(123.45))
+#print(type(True))
+
+print("Number of letters in your name:\n " + str(len(input("Enter your name"))))
