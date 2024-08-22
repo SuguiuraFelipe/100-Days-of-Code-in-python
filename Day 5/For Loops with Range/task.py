b9895133812946bc4d7cecc48f72cea9a39dc660
Felipe Suguiura <felipesuguiura.pro@gmail.com>
2024-08-22 10:46:52 +0100

    day 5

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,18 @@
+# Range
+#for number in range(1, 10):
+#    print(number)
+
+#total = 0
+#for number in range(1, 101):
+#    total += number
+#print(total)
+
+for number in range (1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print ("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,17 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Números aleatórios
+random_letters = random.choices(letters, k = nr_letters)
+random_symbols = random.choices(symbols, k = nr_symbols)
+random_numbers = random.choices(numbers, k = nr_numbers)
 
+# Combinando os caracteres
+password_list = random_letters + random_symbols + random_numbers
+
+# Embaralhando as letras
+random.shuffle(password_list)
+
+# Conversão da lista em string
+password = "".join(password_list)
+print(password)
